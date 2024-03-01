1ae8f32aafc7c1a9b32130f50590e3cb6c3039f6
Davide05 <huzsdavide@gmail.com>
2024-03-01 08:16:51 +0000

    update

--- mysql/calcio.sql
+++ mysql/calcio.sql
@@ -1,23 +1,14 @@
 CREATE Database IF NOT EXISTS Calcio;
 USE Calcio;
-
-
--- Creazione della tabella "Libri"
-CREATE TABLE IF NOT EXISTS Libri (
-    ISBN VARCHAR(13) PRIMARY KEY,
-    Titolo VARCHAR(100),
-    Autore VARCHAR(50),
-    AnnoPubblicazione INT,
-    QuantitaInMagazzino INT
-);
-
+-- Creazione della tabella "Squadra"
 CREATE TABLE  IF NOT EXISTS Squadra (
     codice_squadra int AUTO_INCREMENT PRIMARY KEY NOT NULL,
     codice_capitano int
 );
 SHOW TABLES;
 
-CREATE TABLE Calciatori IF NOT EXISTS
+-- Creazione della tabella "Calciatori"
+CREATE TABLE IF NOT EXISTS Calciatori (
 codice_giocatore int AUTO_INCREMENT PRIMARY KEY NOT NULL,
 cognome varchar, 
 ruolo int,
@@ -25,10 +16,14 @@ stipendio int,
 data_nascita date,
 codice_voto int,
 codice_capitano int,
+);
+SHOW TABLES;
 
-
-CREATE TABLE Valutazioni IF NOT EXISTS 
+-- Creazione della tabella "Valutazioni"
+CREATE TABLE IF NOT EXISTS Valutazioni ( 
 codice_voto int AUTO_INCREMENT PRIMARY KEY NOT NULL, 
 voto int,
 data_partita int,
 FOREIGN KEY(codice_voto(FK))REFERENCES(codice_giocatore(PK)),
+);
+SHOW TABLES;
