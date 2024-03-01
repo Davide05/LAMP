d7469b648a348a671a638645c80040e380343c62
Davide05 <huzsdavide@gmail.com>
2024-03-01 08:47:52 +0000

    update

--- mysql/calcio.sql
+++ mysql/calcio.sql
@@ -1,29 +1,30 @@
 CREATE Database IF NOT EXISTS Calcio;
 USE Calcio;
-
 -- Creazione della tabella "Squadra"
 CREATE TABLE  IF NOT EXISTS Squadra (
-    codice_squadra int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    codice_capitano int
+    codice_squadra INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    codice_capitano INT
 );
 
 -- Creazione della tabella "Calciatori"
 CREATE TABLE IF NOT EXISTS Calciatori (
 codice_giocatore int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-cognome varchar(64), 
+cognome VARCHAR(64), 
 ruolo INT,
 stipendio INT,
 data_nascita DATE,
 codice_voto INT,
-codice_capitano INT
+codice_capitano INT,
 FOREIGN KEY(codice_squadra)REFERENCES Squadra(codice_squadra)
 );
 
 -- Creazione della tabella "Valutazioni"
 CREATE TABLE IF NOT EXISTS Valutazioni ( 
-codice_voto int AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-voto int,
-data_partita int,
-FOREIGN KEY(codice_giocatore)REFERENCES Calciatori(codice_giocatore)
+codice_voto INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
+voto INT,
+data_partita INT,
+/*codice_giocatore INT,*/
+FOREIGN KEY(codice_voto)REFERENCES Calciatori(codice_giocatore)
 );
 SHOW TABLES;
+
