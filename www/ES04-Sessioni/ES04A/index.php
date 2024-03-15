87141fb715b5b37f7f8953b57ef3604084091dfb
Davide05 <huzsdavide@gmail.com>
2024-03-15 08:59:15 +0000

    update

--- www/ES04-Sessioni/ES04A/index.php
+++ www/ES04-Sessioni/ES04A/index.php
@@ -1,1 +1,14 @@
-<?// pagina di benvenuto che va su login ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Benvenuto</title>
+</head>
+<body>
+  <h1>WEBAPP1</h1>
+  <h2>Gestione delle sessioni nelle applicazioni web</h2>
+  <p>
+    <a href="riservata.php">Vai all'area riservata del sito</a><br/>
+    <a href="login.php">Login</a>
+  </p>
+</body>
+</html>
