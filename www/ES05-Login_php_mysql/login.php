d04bbb2894d21836853611648e3cc6b0e0569f70
Davide05 <huzsdavide@gmail.com>
2024-04-05 07:35:15 +0000

    update

---
+++ www/ES05-Login_php_mysql/login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+// Verifica se l'utente è già autenticato e reindirizza alla pagina di benvenuto
+if ( isset($_SESSION["username"]) ) {
+    header('Location: riservata.php');
+    exit;
+}
+
+// Verifica se il modulo di accesso è stato inviato
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST["username"] ?? "";
+    $password = $_POST["password"] ?? ""; 
+
+    // Verifica se le credenziali sono corrette
+    if ($password === "password") {
+        // Credenziali corrette, crea una variabile di sessione per l'autenticazione
+        $_SESSION["username"] = $username;
+        header('Location: riservata.php');
+        exit;
+    } else {
+        echo 'Credenziali non valide. Riprova.';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Accesso</h1>
+
+    <form method="POST" action="">
+        <label for="username">Nome utente:</label>
+        <input type="text" name="username" required><br><br>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br><br>
+
+        <input type="submit" value="Accedi">
+    </form>
+</body>
+</html>
