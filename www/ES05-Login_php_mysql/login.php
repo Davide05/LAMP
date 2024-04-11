8b40597a75625f4c2d6ef1117d668d688bc8cb90
Davide05 <huzsdavide@gmail.com>
2024-04-11 09:17:13 +0000

    update

--- www/ES05-Login_php_mysql/login.php
+++ www/ES05-Login_php_mysql/login.php
@@ -1,30 +1,8 @@
 <?php
 session_start();
 
-// Costanti per la connessione al database
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'db_user');
-define('DB_PASSWORD', 'db_pwd');
-define('DB_NAME', 'nome_database');
-
-// Connessione al database
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-// Verifica della connessione
-if (!$conn) {
-    die("Connessione fallita: " . mysqli_connect_error());
-}
-
-echo "Connessione al database riuscita";
-// ... Puoi eseguire le tue query qui ...
-
-// Chiudere la connessione quando non è più necessaria
-mysqli_close($conn);
-
-
-
 // Verifica se l'utente è già autenticato e reindirizza alla pagina di benvenuto
-if ( isset($_SESSION["username"]) ) {
+if (isset($_SESSION["username"])) {
     header('Location: riservata.php');
     exit;
 }
@@ -34,14 +12,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST["username"] ?? "";
     $password = $_POST["password"] ?? ""; 
 
-    // Verifica se le credenziali sono corrette
+    // Verifica se le credenziali sono corrette (Questo è solo un esempio dimostrativo)
     if ($username === 'davide' && $password === "888") {
         // Credenziali corrette, crea una variabile di sessione per l'autenticazione
         $_SESSION["username"] = $username;
         header('Location: riservata.php');
         exit;
     } else {
-        echo 'Credenziali non valide. Riprova.';
+        $error_message = 'Credenziali non valide. Riprova.';
     }
 }
 ?>
@@ -53,6 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 <body>
     <h1>Accesso</h1>
+    <?php if (isset($error_message)) echo '<p style="color: red;">' . $error_message . '</p>'; ?>
     <h4>Credenziali:</h4>
     <h4>username: davide</h4>
     <h4>password: 888</h4>
