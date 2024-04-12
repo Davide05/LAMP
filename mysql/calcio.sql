d3fb869514faf903dee6717bc8f6fd2500d0804f
Davide05 <huzsdavide@gmail.com>
2024-04-12 07:10:31 +0000

    update

--- mysql/calcio.sql
+++ mysql/calcio.sql
@@ -1,78 +1,115 @@
-CREATE DATABASE IF NOT EXISTS Calcio;
-USE Calcio;
--- Creazione della tabella "Squadra"
-CREATE TABLE IF NOT EXISTS Squadra (
+-- Creazione del database
+CREATE DATABASE IF NOT EXISTS Campionato;
+USE Campionato;
+
+-- Creazione della tabella "Squadre"
+CREATE TABLE squadre (
     nome_squadra VARCHAR(64) NOT NULL,
-    codice_squadra INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    codice_capitano INT
+    ID_squadra INT NOT NULL AUTO_INCREMENT PRIMARY KEY
 );
 SHOW TABLES;
+
+-- Inserimento di dati nella tabella "Squadre"
+INSERT INTO squadre (nome_squadra, ID_squadra) VALUES
+('Napoli', 7),
+('Bologna', 8),
+('Roma', 9),
+('Verdi', 1),
+('Verona', 2);
 
 -- Creazione della tabella "Calciatori"
-CREATE TABLE IF NOT EXISTS Calciatori (
-codice_giocatore int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-cognome VARCHAR(64), 
-ruolo INT,
-stipendio INT,
-data_nascita DATE,
-codice_voto INT,
-codice_capitano INT,
-FK_Squadra int,
-FOREIGN KEY(FK_Squadra)REFERENCES Squadra(codice_squadra)
+CREATE TABLE calciatori (
+    cognome VARCHAR(64),
+    ruolo VARCHAR(64),
+    stipendio INT,
+    ID_calciatore INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ID_capitano INT,
+    nascita DATE,
+    FK_squadre INT,
+    FOREIGN KEY (FK_squadre) REFERENCES squadre (ID_squadra)
 );
 SHOW TABLES;
+
+-- Inserimento di dati nella tabella "Calciatori"
+INSERT INTO calciatori (cognome, ruolo, stipendio, ID_calciatore, ID_capitano, nascita, FK_squadre) VALUES
+('Bianchi', 'terzino', 10000, 3, 1, '2005-05-10', 7),
+('Zeng', 'attaccante', 15000, 18, 4, '1998-01-09', 8),
+('Hu', 'attaccante', 9000, 10, 1, '2000-04-23', 1),
+('Gialli', 'difensore', 20000, 8, 3, '2002-09-27', 1),
+('Giolitti', 'difensore', 25000, 2, 3, '2001-11-29', 2),
+('Castro', 'portiere', 12000, 5, 5, '1999-12-30', 9);
 
 -- Creazione della tabella "Valutazioni"
-CREATE TABLE IF NOT EXISTS Valutazioni ( 
-codice_voto INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-voto INT,
-data_partita INT,
-FK_Calciatore int,
-FOREIGN KEY(FK_Calciatore)REFERENCES Calciatori(codice_giocatore)
+CREATE TABLE valutazioni (
+    voto INT,
+    ID_valutazione INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    FK_calciatori INT,
+    FOREIGN KEY (FK_calciatori) REFERENCES calciatori (ID_calciatore)
 );
 SHOW TABLES;
 
+-- Inserimento di dati nella tabella "Valutazioni"
+INSERT INTO valutazioni (voto, ID_valutazione, FK_calciatori) VALUES
+(8, 1, 18),
+(7, 2, 3),
+(6, 3, 8),
+(9, 4, 2),
+(5, 5, 5);
+
+
 /*
-1)modifica il campo calciatori, cambia la variabile di ruolo
-ALTER TABLE Calciatori 
+1)QUERY che modifica il campo 'Calciatori'
+ALTER TABLE calciatori 
 CHANGE COLUMN ruolo ruolo VARCHAR(64);
+select*from calciatori;
 
 2)inserire istanza nella tabella calciatori
-INSERT INTO Calciatori (ruolo)
+INSERT INTO calciatori (ruolo)
 VALUES ('attaccante');
+select*from calciatori;
 
 3) modificare nella tabella calciatori il cognome Rossi con il cognome Bianchi
-UPDATE Calciatori
-SET cognome='Bianchi'
-WHERE cognome='Rossi';
+UPDATE calciatori
+SET cognome='Rossi'
+WHERE cognome='Bianchi';
+select*from calciatori;
 
-4)cancellare nella tabella quadre l'istanza con nome squadra=Verdi
-DELETE FROM Squadra
+
+4)cancellare nella tabella squadre l'istanza con nome squadra=Verdi
+DELETE FROM squadre
 WHERE nome_squadra='Verdi';
+select*from squadre;
 
 5)produrre un elenco con i cognomi di tutti i calciatori aventi stipendio>10.000 oridnato su cognome
 SELECT cognome 
 FROM calciatori
-WHERE stipendio>10.000
+WHERE stipendio>10000
 ORDER BY cognome;
 
-6)visualizzare tuitte le informazioni dei calciatori che ricoporono ruolo 'terzino' oi 'portiere'
+
+6)visualizzare tutte le informazioni dei calciatori che ricoporono ruolo 'terzino' o i 'portiere'
 SELECT * 
 FROM calciatori
-WHERE ruolo='terzino'or'portiere';
+WHERE ruolo = 'terzino' OR ruolo = 'portiere';
 
-7)visualzzare cognome di tutti i calciatori la cui lettera 2 del cognome è 'a' e lultima lettera è 'o'
+7)visualzzare cognome di tutti i calciatori la cui lettera 2 del cognome è 'a' e l'ultima lettera è 'o'
 SELECT cognome
 FROM calciatori
 WHERE cognome like '_a%o';
 
 8)produrre elenco con i cognomi dei calciatori seguiti dal nome della loro sqaudra solo per le squadre Napoli, Bologna, Verona (join con where)
-SELECT cognome, nome_squadra
+SELECT c.cognome, s.nome_squadra
+FROM calciatori c
+JOIN squadre s ON c.FK_squadre = s.ID_squadra
+WHERE s.nome_squadra IN ('Napoli', 'Bologna', 'Verona');
 
 9)visualizzare qaunti calciatori sono nati prima del 2000
+SELECT COUNT(*) 
+FROM calciatori
+WHERE YEAR(nascita) < 2000;
 
+10)visulaizzare solo una volta in ordine crescente tutti i voti nella tabella valutazioni
 
-10)visulaizzare solo una volta in rodijne crescente tutti i voti nella tabella valutazioni
 11)visualizzare il voto più alto, più basso e la media di tutti i voti della tabella valutazioni
 12)per ogni calciatore visulazziare il cognome seguito dal voto massimo, dal voto minimo e dalla media dei voti e da qaunti voti ha ottenuto nelle partite che ha disputato
 13)visualizzare il cognome di ogni giocatore seguito dal cognome del suo capitano
